09d4df3b04ab8882a9a28903e0c9d653adbc6272
jstoaks2112 <jstoaks2112@gmail.com>
2024-09-29 09:31:18 -0600

    Add files via upload

---
+++ hadpms.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client as mqtt
+import subprocess
+
+# MQTT Settings
+mqtt_broker = "ha.stoaks.net"
+mqtt_port = 1883
+mqtt_topic = "/homeassistant/sensor/vaio/lcd"
+mqtt_username = "john"
+mqtt_password = "tool2112"
+
+# Define callback functions
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT Broker with result code "+str(rc))
+    client.subscribe(mqtt_topic)
+
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode("utf-8")
+    print(f"Received message on topic {msg.topic}: {payload}")
+
+    if payload == "ON":
+        subprocess.call(["xset", "dpms", "force", "on"])
+        subprocess.call(["xset", "s", "noblank"])
+        subprocess.call(["xset", "s", "off"])
+        subprocess.call(["xset", "-dpms"])
+    elif payload == "OFF":
+        subprocess.call(["xset", "dpms", "force", "off"])
+
+# Create MQTT client
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Set MQTT username and password
+client.username_pw_set(mqtt_username, mqtt_password)
+
+# Connect to MQTT Broker
+client.connect(mqtt_broker, mqtt_port, 60)
+
+# Start the MQTT loop
+client.loop_forever()
+
+
